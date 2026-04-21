2a36e5b738141e3ef339f43fa945dbdb04b60e86
Preetham M.S <62534652+PreethamMSalpha@users.noreply.github.com>
2026-04-21 17:09:17 +0530

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[][]} times
+     * @param {number} n
+     * @param {number} k
+     * @return {number}
+     */
+    networkDelayTime(times, n, k) {
+        // step1 -> build adjacency list
+        let adj = new Map(); // source -> [destination, weight]
+
+        for(const [u,v,w] of times){
+            if(!adj.has(u)) adj.set(u, [])
+            adj.get(u).push([v,w])
+        }
+
+        // step2 -> build minHeap and distance map
+        let minHeap = [[0, k]] // [distance, node]
+        let dist = new Map();
+
+        while(minHeap.length){
+            // replicate minHeap by sorting now
+            minHeap.sort((a,b) => a[0] - b[0]);
+
+            const [d, node] = minHeap.shift();
+
+            if(dist.has(node)) continue; // skip already visited node
+
+            dist.set(node, d);
+
+            // explore the neighbours of node
+            for(const [n, w] of adj.get(node) || []){
+                console.log([n,w])
+                minHeap.push([d + w, n]);
+            }
+        }
+
+        // length check - all nodes are visited or not
+        if(dist.size != n) return -1;
+
+        return Math.max(...dist.values());
+    }
+}
